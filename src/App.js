815db0cbb4bda5d6724a17951cef4cd185b1edc2
imranshaikh64 <imranhshaikh1@gmail.com>
2021-10-05 19:51:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         let winner = checkWinner();
         if (winner) {
             clearGame();
-            alert(`Congrats !!! ${winner} won the Game !`)
+            alert(`Congratulations !!! ${winner} won the Game !`)
         }
     }, [gameState])
 
